11e82c83f2ff43d7bc19df7d70152fac6fb4a383
Nachop51 <nachoperalta0@gmail.com>
2023-10-03 17:44:16 -0300

    add: test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
+"""
+Django admin customization
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -37,3 +37,13 @@ class ModelTests(TestCase):
         ''' Test creating user with no email raises error. '''
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
